86ee00ff7a986749b1f584b0a5c6435620534bc1
James Beston <james@bigpixel.org.uk>
2026-05-29 19:42:21 +0100

    Update hero-video.tsx

--- src/components/home/hero-video.tsx
+++ src/components/home/hero-video.tsx
@@ -5,10 +5,10 @@ export default function HeroVideo() {
         <div className="h-screen w-screen flex items-end lg:items-center">
           <div className="w-full max-w-7xl mx-auto p-4 lg:p-6 items-start">
             <div className="w-full lg:w-2/3 flex flex-col gap-y-4 md:gap-y-6 lg:gap-y-9 xl:gap-y-12">
-              <h1 className="font-headline font-black text-7xl lg:text-[5.5rem] 2xl:text-[10rem] text-white text-shadow-black/10 text-shadow-lg text-balance">
+              <h1 className="font-headline font-black text-7xl lg:text-[5.5rem] 2xl:text-[10rem] text-white">
                 Look as good as the work you do
               </h1>
-              <p className="text-xl md:text-2xl lg:text-3xl 2xl:text-5xl text-white text-shadow-black/10 text-shadow-lg text-balance leading-tight">
+              <p className="text-xl md:text-2xl lg:text-3xl 2xl:text-5xl text-white leading-tight">
                 Digital strategy, design, and web development for charities, non-profits &amp;
                 social enterprises.
               </p>
